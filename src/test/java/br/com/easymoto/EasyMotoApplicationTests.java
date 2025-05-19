a203550a25ce809e97eef8c2d5c30fab06af3772
akemilol <valeriaconcedsantos@gmail.com>
2025-05-19 13:39:25 -0300

    Criação das pastas, java

---
+++ src/test/java/br/com/easymoto/EasyMotoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easymoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
