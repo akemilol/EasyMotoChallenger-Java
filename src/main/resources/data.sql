87bfbfe5d60912b2ba91c59d7abfd99143c43155
akemilol <valeriaconcedsantos@gmail.com>
2025-05-19 17:58:19 -0300

    adição do dockerfile, configuração dos inserts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Cliente (id_cliente, nome_cliente, cpf_cliente, telefone_cliente, email_cliente) VALUES (1, 'João Silva', '12345678901', '11988880001', 'joao@email.com');
+INSERT INTO Cliente (id_cliente, nome_cliente, cpf_cliente, telefone_cliente, email_cliente) VALUES (2, 'Maria Souza', '23456789012', '11988880002', 'maria@email.com');
+INSERT INTO Cliente (id_cliente, nome_cliente, cpf_cliente, telefone_cliente, email_cliente) VALUES (3, 'Pedro Santos', '34567890123', '11988880003', 'pedro@email.com');
+INSERT INTO Cliente (id_cliente, nome_cliente, cpf_cliente, telefone_cliente, email_cliente) VALUES (4, 'Ana Costa', '45678901234', '11988880004', 'ana@email.com');
+INSERT INTO Cliente (id_cliente, nome_cliente, cpf_cliente, telefone_cliente, email_cliente) VALUES (5, 'Lucas Lima', '56789012345', '11988880005', 'lucas@email.com');
+
+INSERT INTO Empresa (id_empresa, nome_empresa, cnpj) VALUES (1, 'Mottu Ltda', '12345678000191');
+INSERT INTO Empresa (id_empresa, nome_empresa, cnpj) VALUES (2, 'Alpha Motos', '23456789000191');
+INSERT INTO Empresa (id_empresa, nome_empresa, cnpj) VALUES (3, 'Beta Motors', '34567890000191');
+INSERT INTO Empresa (id_empresa, nome_empresa, cnpj) VALUES (4, 'Gamma Riders', '45678900000191');
+INSERT INTO Empresa (id_empresa, nome_empresa, cnpj) VALUES (5, 'Omega Bikes', '56789000000191');
